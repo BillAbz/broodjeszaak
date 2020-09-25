bbc87ad8dca95b7cf081bf2aa5a97b5fe272aea7
BillAbz <68382453+BillAbz@users.noreply.github.com>
2020-09-25 15:15:31 +0200

    promoties pagina

--- Harika/bzscript.js
+++ Harika/bzscript.js
@@ -381,3 +381,15 @@ function aantal_kiezen() {
 
 }
 
+$(function () {
+    var selectedClass = "";
+    $(".filter").click(function () {
+      selectedClass = $(this).attr("data-rel");
+      $("#gallery").fadeTo(100, 0.1);
+      $("#gallery div").not("." + selectedClass).fadeOut().removeClass('animation');
+      setTimeout(function () {
+        $("." + selectedClass).fadeIn().addClass('animation');
+        $("#gallery").fadeTo(300, 1);
+      }, 300);
+    });
+  });
